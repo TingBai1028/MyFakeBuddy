35c6871c42fa2ca8e3679f3ca51dd9ba292db467
Ting <baiyuting652@gmail.com>
2025-01-25 11:14:48 +1100

    add not fount page

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+    return (
+        <>
+            <Stack.Screen options={{ title: "Page Not found" }} />
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                Go Back to Home Screen!
+                </Link>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff'
+    }
+});
